aefb9c2d3bdf14d2b58013e06879364fc5b1ddc1
huan00 <96999144+huan00@users.noreply.github.com>
2023-08-30 11:13:15 -0400

    setup tailwind

---
+++ src/services/index.tsx
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+export const getWeather = async (location: {
+  latitude: number
+  longitude: number
+}) => {
+  const res = await axios.get(
+    `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location.latitude},${location.longitude}`
+  )
+  return res.data
+}
