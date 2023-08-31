83a54004b09fd36119fe02a14b2ca60138b35536
huan00 <96999144+huan00@users.noreply.github.com>
2023-08-31 12:22:27 -0400

    connect with backend api to generate outfit

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -7,8 +7,22 @@ export const getWeather = async (location: {
   longitude: number
 }) => {
   const res = await axios.get(
-    // `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${location.latitude},${location.longitude}`
-    `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location.latitude},${location.longitude}`
+    `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location.latitude},${location.longitude}&day=3`
   )
+  return res.data
+}
+
+export const getMyOutfit = async (weather: {
+  temperature_high: string
+  temperature_low: string
+  wind: string
+  humidity: string
+  condition: string
+}) => {
+  const res = await axios.post(
+    `http://localhost:8000/warm-weather/user/getmyoutfit`,
+    weather
+  )
+
   return res.data
 }
